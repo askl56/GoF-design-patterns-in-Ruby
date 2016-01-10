175d931a5887d6de00c52c56fa8b19dd668b307a
Andrew Scott <amscott@openmailbox.org>
2016-01-10 17:56:08 +0000

    Added gemfile

--- template/report.rb
+++ template/report.rb
@@ -7,28 +7,20 @@ class Report
   def output_report
     output_start
     output_head
-    output_body_start
-    output_body
-    output_body_end
-    output_end
-  end
-
-  def output_body
     @text.each do |line|
       output_line(line)
     end
+    output_end
   end
 
   def output_start
-    raise 'Called abstract method: output_start'
   end
 
   def output_head
-    raise 'Called abstract method: output_head'
+    output_line(@title)
   end
 
   def output_body_start
-    raise 'Called abstract method: output_body_start'
   end
 
   def output_line(_line)
@@ -36,10 +28,8 @@ class Report
   end
 
   def output_body_end
-    raise 'Called abstract method: output_body_end'
   end
 
   def output_end
-    raise 'Called abstract method: output_end'
   end
 end
